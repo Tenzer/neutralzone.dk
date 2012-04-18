cae120f504b9ec628429f70987a2256bace3da53
Jeppe Toustrup <tenzer@tenzer.dk>
2012-04-18 23:41:33 +0200

    Major progress, basically all features are in place, only design is missing

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-
-/**
- * Module dependencies.
+/*jshint
+  node: true,
+  globalstrict: true,
+  laxcomma: true
  */
 
+'use strict';
+
+/* Module dependencies */
+
 var express = require('express');
+var https = require('https');
 
 var app = module.exports = express.createServer();
 
-// Configuration
+
+/* Configuration */
 
 app.configure(function () {
   app.set('views', __dirname + '/views');
@@ -26,7 +33,8 @@ app.configure('production', function () {
   app.use(express.errorHandler());
 });
 
-// Routes
+
+/* Routes */
 
 app.get('/', function (req, res) {
   res.render('index', { layout: false, title: 'Live tweets from Danish American Football teams' });
@@ -35,6 +43,9 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Express server listening on port %d in %s mode', app.address().port, app.settings.env);
 });
+
+
+/* Socket.IO */
 
 var clients = 0;
 var io = require('socket.io').listen(app);
@@ -45,12 +56,39 @@ io.sockets.on('connection', function (socket) {
   socket.emit('users', { count: clients });
   socket.broadcast.emit('users', { count: clients });
 
+  // Sends out the latest tweets to new users
+  for (var i = 0; i < latest_tweets.length; i++) {
+    socket.emit('tweet', latest_tweets[i]);
+  }
+
   socket.on('disconnect', function () {
     clients--;
     socket.broadcast.emit('users', { count: clients });
   });
 });
 
-setInterval(function () {
-  io.sockets.emit('tweet', { time: new Date() });
-}, 5000);
+
+/* Twitter */
+
+var twitter_options = JSON.parse(require('fs').readFileSync('twitter_options.json'));
+
+var latest_tweets = [];
+var Twitter = require('./twitter_streaming.js');
+var t = new Twitter(twitter_options);
+
+t.on('tweet', function (tweet) {
+  io.sockets.emit('tweet', tweet);
+
+  if (latest_tweets.push(tweet) > 20) {
+    latest_tweets.shift();
+  }
+});
+
+t.on('error', function (e) {
+  console.error('ERROR: Received following error message from Twitter handler:');
+  console.error(e.message);
+  console.error('Quitting now!');
+  require('process').exit(1);
+});
+
+t.getTweets();
---
+++ twitter_streaming.js
@@ -0,0 +1,70 @@
+/*jshint
+  laxcomma: true
+*/
+
+//
+// Based on https://gist.github.com/833401
+//
+
+var util = require('util'),
+  https = require('https'),
+  events = require('events');
+
+var Twitter = function (opts) {
+  this.username = opts.username;
+  this.password = opts.password;
+  this.track = opts.track.join(',');
+  this.data = '';
+};
+
+Twitter.prototype = new events.EventEmitter();
+
+Twitter.prototype.getTweets = function () {
+  var opts = {
+    hostname: 'stream.twitter.com'
+  , path: '/1/statuses/filter.json'
+  , method: 'POST'
+  , headers: {
+      'Connection': 'keep-alive'
+    , 'Authorization': 'Basic ' + new Buffer(this.username + ':' + this.password).toString('base64')
+    }
+  };
+  var self = this;
+
+  opts.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+  opts.headers['Content-Length'] = ('track=' + this.track).length;
+
+  this.connection = https.request(opts, function(response) {
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      self.data += chunk.toString('utf8');
+
+      var index, json;
+
+      while ((index = self.data.indexOf('\r\n')) > -1) {
+        json = self.data.slice(0, index);
+        self.data = self.data.slice(index + 2);
+        if (json.length > 0) {
+          try {
+            self.emit('tweet', JSON.parse(json));
+          } catch (e) {
+            self.emit('error', e);
+          }
+        }
+      }
+    });
+  });
+
+  this.connection.on('error', function (e) {
+    self.emit('error', e);
+  });
+
+  this.connection.on('end', function () {
+    self.emit('error', { message: 'Connection closed' });
+  });
+
+  this.connection.write('track=' + this.track);
+  this.connection.end();
+};
+
+module.exports = Twitter;
