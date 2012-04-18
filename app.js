88f84e4c04a0b5f3fcb6f0e3f4e97a8124adcbad
Jeppe Toustrup <tenzer@tenzer.dk>
2012-04-18 09:12:09 +0200

    Added functionality to show the amount of currently connected users

--- app.js
+++ app.js
@@ -29,18 +29,27 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { layout: false, title: 'Live tweets from Danish American Football teams' });
 });
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
+var clients = 0;
 var io = require('socket.io').listen(app);
+
 io.sockets.on('connection', function (socket) {
-  socket.emit('towers', { hello: 'world' });
+  clients++;
+  socket.emit('users', { count: clients });
+  socket.broadcast.emit('users', { count: clients });
+
+  socket.on('disconnect', function () {
+    clients--;
+    socket.broadcast.emit('users', { count: clients });
+  });
 });
 
 setInterval(function () {
-  io.sockets.emit('towers', {towers: 'are the best!'});
-}, 1000);
+  io.sockets.emit('tweet', { time: new Date() });
+}, 5000);
