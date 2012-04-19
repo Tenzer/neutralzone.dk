c5d3f911aae8c0e4dda9a09274008365ff6f1fa8
Jeppe Toustrup <=>
2012-04-19 11:11:30 +0200

    Decreased log level from Socket.io slightly

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.listen(3000, function () {
 var clients = 0;
 var io = require('socket.io').listen(app);
 io.enable('browser client minification');
+io.set('log level', 2);
 
 io.sockets.on('connection', function (socket) {
   clients++;
