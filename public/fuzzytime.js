33510280f95992cec26fdf56d44478235328195a
Jeppe Toustrup <tenzer@tenzer.dk>
2012-04-24 17:12:01 +0200

    Added line break for readability

--- public/fuzzytime.js
+++ public/fuzzytime.js
@@ -1,5 +1,6 @@
 function goFuzzy (now) {
   var list = document.body.getElementsByTagName('time');
+
   for (var i = 0; i < list.length; i++) {
     var time = Date.parse(list[i].getAttribute('datetime'));
     var diff = Math.round((now - time) / 1000);
