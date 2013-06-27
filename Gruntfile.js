602edd6e8db5cc3fa23d4c34fa25e18725758b57
Jeppe Toustrup <jeppe@tenzer.dk>
2013-06-27 09:27:14 +0200

    Converted all indentation to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,30 @@
 module.exports = function (grunt) {
-	'use strict';
+  'use strict';
 
-	grunt.initConfig({
-		reduce: {
-			root: 'public',
-			outRoot: 'dist',
-			less: false
-		},
-		handlebars: {
-			options: {
-				namespace: 'Handlebars.templates',
-				processName: function (filename) {
-					var pieces = filename.split('/');
-					return pieces[pieces.length - 1].replace(/\.handlebars$/, '');
-				}
-			},
-			compile: {
-				files: {
-					'public/js/tweet.handlebars.js': 'templates/tweet.handlebars'
-				}
-			}
-		}
-	});
+  grunt.initConfig({
+    reduce: {
+      root: 'public',
+      outRoot: 'dist',
+      less: false
+    },
+    handlebars: {
+      options: {
+        namespace: 'Handlebars.templates',
+        processName: function (filename) {
+          var pieces = filename.split('/');
+          return pieces[pieces.length - 1].replace(/\.handlebars$/, '');
+        }
+      },
+      compile: {
+        files: {
+          'public/js/tweet.handlebars.js': 'templates/tweet.handlebars'
+        }
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-reduce');
-	grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-reduce');
+  grunt.loadNpmTasks('grunt-contrib-handlebars');
 
-	grunt.registerTask('default', ['handlebars', 'reduce']);
+  grunt.registerTask('default', ['handlebars', 'reduce']);
 };
